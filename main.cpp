f0cd4de893994c90ec50b386fe567604210c05e6
Artem Chepushtanov <che-art-and07@mail.ru>
2026-09-19 14:36:35 +0700

    Добавлена вторая расчётная формула и пункт меню

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 using namespace std;
 
 // ===== Расчётная функция варианта =====
+
 // Кинетическая энергия тела
 double kineticEnergy(double m, double v) {
     return (m * v * v) / 2.0;
+}
+
+// Импульс тела
+double momentum(double m, double v) {
+    return (m * v);
 }
 
 // ===== Главная функция: меню =====
@@ -16,6 +22,7 @@ int main() {
     do {
         cout << "\n=== Вариант 24: Кинетическая энергия ===\n";
         cout << "1. Рассчитать кинетическую энергию\n";
+	cout << "2. Рассчитать импульс\n";
         cout << "0. Выход\n";
         cout << "Выберите пункт: ";
         cin >> choice;
@@ -25,6 +32,11 @@ int main() {
                 cin >> m >> v;
                 cout << "Кинетическая энергия = " << kineticEnergy(m, v) << "\n";
                 break;
+	    case 2:
+		cout << "Введите массу (m) и скорость (v): ";
+		cin >> m >> v;
+		cout << "Импульс = " << momentum(m, v) << "\n";
+		break;
             case 0:
                 cout << "Работа завершена.\n";
                 break;
